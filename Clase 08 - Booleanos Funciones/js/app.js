01cbb290674dfe9dacc2e1cc7cdc045d36fb0960
Matías Fernández <mfernandez.1992@icloud.com>
2021-11-21 17:47:03 -0300

    .

---
+++ Clase 08 - Booleanos Funciones/js/app.js
@@ -0,0 +1,25 @@
+function transferenciaBancaria(monto, cuentaDestino, cuentaOrigen) {
+    // Confirmo si cuentaDestino tiene el monto suficiente
+    // Efectuó la transferencia
+
+    return 'Transferencia OK';
+}
+
+transferenciaBancaria(10000,'MDEO-00012', 'CAN- 000242');
+
+
+// Ejercicios
+//
+function duplicar(number) {
+    return number * 2;
+}
+
+duplicar(3);
+
+// Definir una función llamda mostrarNombre que reciba como parámetro dos strings cualesquiera y despligue un alert conteniendo el texto ""Tu nombre es María y tu apellido es Pérez"
+
+function mostrarNombre(nombre, apellido) {
+    alert('Tu nombre es ' + nombre + 'y tu apellido es ' + apellido);
+}
+
+mostrarNombre('Matías', 'Fernández');
