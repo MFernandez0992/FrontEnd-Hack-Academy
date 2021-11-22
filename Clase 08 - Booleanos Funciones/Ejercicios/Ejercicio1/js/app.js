3b85e3380509069611dabe8fa93649d954b70d81
Matías Fernández <mfernandez.1992@icloud.com>
2021-11-22 10:53:39 -0300

    Resolución ejercicios

--- Clase 08 - Booleanos Funciones/Ejercicios/Ejercicio1/js/app.js
+++ Clase 08 - Booleanos Funciones/Ejercicios/Ejercicio1/js/app.js
@@ -9,3 +9,35 @@ function esPar(numero){
 }
 
 esPar(4);
+
+function maximoDeDos(number1, number2) {
+    if (number1 > number2) {
+        console.log(number1);
+    } else{
+        console.log(number2);
+    } if (number1 === number2) {
+        console.log(number1);
+    }
+}
+
+maximoDeDos(8, 8);
+
+function validarNombre(nombre) {
+    if ((typeof nombre === "") && (nombre !== "")) {
+        console.log("Nombre es un string")
+    } else {
+        console.log("Nombre no es un string o está vacío")
+    }
+}
+
+validarNombre("1");
+
+function validarAnioNacimiento(anio) {
+    if ((anio > 1920) &&(anio <= 2021)) {
+        console.log("El año es entre 1920 y 2021");
+    } else {
+        console.log("El año es menor a 1920 o mayor a 2021");
+    }
+}
+
+validarAnioNacimiento(1929);
