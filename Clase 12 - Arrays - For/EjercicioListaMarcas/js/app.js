6d0359ee1bcd4bfc4b96739c80e4ee9e121d6747
Matías Fernández <mfernandez.1992@icloud.com>
2021-11-29 16:55:30 -0300

    Ejercicio Marcas

--- Clase 12 - Arrays - For/EjercicioListaMarcas/js/app.js
+++ Clase 12 - Arrays - For/EjercicioListaMarcas/js/app.js
@@ -1,5 +1,5 @@
 // Declaro el array que contenerá las marcas
-var marcas = [];
+var marcas = ["BMW", "Peugeot", "Chevrolet", "Subaru", "Nissan"];
 var mostrar = $('#mostrar');
 var eliminar = $('#eliminar');
 var agregar = $('#agregar');
@@ -13,6 +13,19 @@ $(agregar).on("click", function () {
 
 */
 
+// Función que mostrará las marcas del array
+$(mostrar).on("click", function mostrarMarcas() {
+    for (let i = 0; i < marcas.length; i++) {
+        // console.log(marcas[i]); Pruebo que la función y el for estén funcionando
+        
+        // Inserto en el ul las marcas, cada una con su li
+        $("#listaMarcas").append('<li>' + marcas[i] + '</li>');
+
+        // Muestro y oculto las marcas PENDIENTE
+        //$("#listaMarcas").toggle();
+    }
+})
+
 // Función que agregará las nuevas marcas al array
 $(agregar).on("click", function () {
     // Extraigo el valor del input y lo almaceno en una variable
@@ -21,12 +34,26 @@ $(agregar).on("click", function () {
     // Declaro una variable que funcione para crear un nuevo li
     var nuevoItem = document.createElement('li');
 
-    // Agrego el nuevo item al array y lo muestro en la lista
-    $(nuevoItem).html(valorInput);
-	marcas.push($("#listaMarcas").append(nuevoItem));
+    // Valido que el valor no exista y lo agrego al array
+
+    if (valorInput !== "") {
+        $(nuevoItem).html(valorInput);
+        marcas.push(valorInput).append(nuevoItem);
+
+    }
 });
 
 // Función para eliminar la última marca
 $(eliminar).on('click', function() {
-    marcas.pop($('#listaMarcas').remove());
+    marcas.pop();
 });
+
+
+// PENDIENTE
+/* 
+    - Que botón mostrar no repite el ul
+    - Que eliminar item refresque el ul
+    - Que agregar marca refresque el ul
+    - Que no se puedan agregar marcas repetidas, si se intenta agregar alguna lanzar un msj de alerta
+    - Agregar campo de texto que permita eliminar la marca que desee, si no existe en el array lanzar msj
+*/
