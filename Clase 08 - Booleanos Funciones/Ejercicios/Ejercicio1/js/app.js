7c14b27e6d3bf2302f8ce10944e88ccbeaec8c94
Matías Fernández <mfernandez.1992@icloud.com>
2021-11-30 16:25:25 -0300

    Ejercicios
    Repaso ejercicios de clase 8

--- Clase 08 - Booleanos Funciones/Ejercicios/Ejercicio1/js/app.js
+++ Clase 08 - Booleanos Funciones/Ejercicios/Ejercicio1/js/app.js
@@ -1,26 +1,58 @@
+// Definir una función llamada duplicar que reciba como parámetro un número cualquiera y retorne su doble.
+
+function duplicar(numero){
+    console.log(numero * 2);
+}
+
+duplicar(6);
+
+
+// Definir una función llamada mostrarNombre que reciba como parámetro dos strings cualesquiera y despliegue un alert conteniendo el texto:“Tu nombre es María y tu apellido es Pérez”(suponiendo que los argumentos ingresados fueron “María” y “Pérez”).
+
+function mostrarNombre(nombre, apellido){
+    alert(`Tu nombre es ${nombre} y tu apellido es ${apellido}`);
+}
+
+mostrarNombre("Matías", "Fernández");
+
+
+// Definir una función llamada numeroAleatorio que retorne un número cualquiera entre 1 y 100. Se puede usar la función Math.random (ya definida en JavaScript).
+
+function numeroAleatorio(){
+    console.log(Math.round(Math.random()));
+}
+numeroAleatorio(); // PENDIENTE
+
+
 // Definir una función llamada esPar que reciba como parámetro un número cualquiera y retorne true si es par, y false si es impar
 
 function esPar(numero){
-    if(numero % 2 == 0){
-        console.log('El número es par')
+    if(numero % 2 === 0){
+        return true;
     }else{
-        console.log('El número es impar')
+        return false;
     }
 }
 
-esPar(4);
+esPar();
 
-function maximoDeDos(number1, number2) {
-    if (number1 > number2) {
-        console.log(number1);
-    } else{
-        console.log(number2);
-    } if (number1 === number2) {
-        console.log(number1);
+
+// Definir una función llamada maximoDeDos que reciba como argumentos dos números cualesquiera y retorne el mayor. Si los números son iguales retornar dicho número.Nota: No utilizar la función Math.max(...).
+
+function maximoDeDos(num1, num2){
+    if(num1 > num2){
+        console.log(num1);
+    }else{
+        console.log(num2)
+    }if(num1 === num2){
+        console.log("Los números son iguales" + num1);
     }
 }
 
-maximoDeDos(8, 8);
+maximoDeDos(2, 7);
+
+
+// Definir una función llamada validarNombre que reciba como parámetro un string cualquiera y retorne true si el argumento recibido efectivamente es un string y además no coincida con el string vacío “”. En caso contrario, retornar false.
 
 function validarNombre(nombre) {
     if ((typeof nombre === "") && (nombre !== "")) {
@@ -30,19 +62,28 @@ function validarNombre(nombre) {
     }
 }
 
-validarNombre("1");
+validarNombre("Hola");
+
+
+/* Definir una función llamada validarAnioNacimiento que reciba como parámetro un número cualquiera y retorne true si:
+- el argumento recibido efectivamente es un número y
+- el argumento recibido es mayor a 1920 y
+- el argumento recibido es menor o igual a 2021.
+En caso contrario, la función debe retornar false.
+*/
 
 function validarAnioNacimiento(anio) {
-    if ((anio > 1920) &&(anio <= 2021)) {
-        console.log("El año es entre 1920 y 2021");
-    } else {
-        console.log("El año es menor a 1920 o mayor a 2021");
+    if(anio > 1920 && anio < 2021){
+        return true;
+    }else{
+        return false;
     }
 }
 
 validarAnioNacimiento(1929);
 
 
+// Calcular el sueldo líquido descontando impuestos
 function sueldoLiquido(sueldoBruto) {
     var descuento = 0.8;
     return sueldoBruto * descuento;
@@ -53,6 +94,7 @@ sueldoLiquido(15000);
 // Si quiero guardar el valor en una variable
 var salario = sueldoLiquido(20000);
 console.log(salario);
+
 
 // Horario de negocio
 
