0dd9dd77f66d3af0aae709be418bf71c764696ac
Matías Fernández <mfernandez.1992@icloud.com>
2021-12-02 15:36:40 -0300

    tutorialVue.js

---
+++ Clase 15 - VueJS/practicaYT/js/app.js
@@ -0,0 +1,15 @@
+var myApp = new Vue({
+    el: '#tutorialYouTubeHA',
+    data: {
+        title: "Tutorial en YouTube de Hack Academy",
+        year: 2021,
+        descripcion: "Tutorial sobre manejo del framework Vue.js",
+        courses: [
+            "Front-End",
+            "Back-End (PHP)",
+            "Back-End (Node.js)",
+            "React.js",
+            "Coding Bootcamp"
+        ]
+    }
+});
