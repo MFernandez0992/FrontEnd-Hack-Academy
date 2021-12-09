ba13fb1cf086b76a6330b0bca64439b74d75ce47
Matías Fernández <mfernandez.1992@icloud.com>
2021-12-08 22:06:18 -0300

    update

--- ProyectoFinal/js/app.js
+++ ProyectoFinal/js/app.js
@@ -6,7 +6,12 @@ var app = new Vue({
         years: [],
         brands: [],
         models: [],
-        condition: []
+        condition: [],
+        selectedBrand: "",
+        selectedModel: "",
+        selectedYear: "",
+        selectedStatus: "",
+        cars: []
     }
     
 });
@@ -14,12 +19,13 @@ var app = new Vue({
 // Guardo las URLs de las api dentro de variables
 var exchange = 'https://ha.edu.uy/api/rates';
 var listBrands = 'https://ha.edu.uy/api/brands';
+// var apiModels = 'https://ha.edu.uy/api/models?brand=' + selectedBrand;
 
 // Carga de Tipo de Cambio:
 $.ajax({
     url: exchange,
-    success: function (response) {
-        app.exchangeRateUyu = response.uyu;
+    success: function (responseExchange) {
+        app.exchangeRateUyu = responseExchange.uyu;
     }
 });
 
@@ -31,7 +37,34 @@ for (var i = 2021; i >= 1900; i--){
 // Cargo las marcas desde la api, guardo los valores de la api dentro de la variable responde de la function
 $.ajax({
     url: listBrands,
-    success: function (response) {
-        app.brands = response;
+    success: function (responseBrands) {
+        app.brands = responseBrands;
     }
 });
+
+// Cargo los modelos dependiendo de la marca seleccionada, con el evento change detecto cuando el usuario de click a una marca y llamo a la api de los modelos
+$('#select-brand').on('change', function(){
+    $.ajax({
+        url: 'https://ha.edu.uy/api/models?brand=' + app.selectedBrand,
+        success: function(responseModels) {
+            app.models = responseModels;
+        }
+    })
+
+})
+
+$('#btn-filter').on('click', function() {
+    var year = app.selectedYear;
+    var modelSelected = app.selectedModel;
+    var brandSelected = app.selectedBrand;
+    var status = app.selectedStatus;
+
+    console.log(year, modelSelected, brandSelected, status);
+
+    $.ajax({
+        url: `https://ha.edu.uy/api/cars?year=${year}&brand=${brandSelected}&model=${modelSelected}&status=${status}`,
+        success: function (cars) {
+            app.cars = cars;
+        }
+    })
+})
